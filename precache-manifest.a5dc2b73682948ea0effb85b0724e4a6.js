7ebc909d69b705e20f3145b02409cf625e44c997
rahul <rahul07vashisht@gmail.com>
2020-06-30 00:49:07 +1000

    Updates

---
+++ precache-manifest.a5dc2b73682948ea0effb85b0724e4a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84821d32f3363f8e250f21692f8f2afe",
+    "url": "/wanderlust/index.html"
+  },
+  {
+    "revision": "2bb031d9348de3bc6303",
+    "url": "/wanderlust/static/css/2.7156d6df.chunk.css"
+  },
+  {
+    "revision": "fba124aedd234ef928ab",
+    "url": "/wanderlust/static/css/main.d15e702f.chunk.css"
+  },
+  {
+    "revision": "2bb031d9348de3bc6303",
+    "url": "/wanderlust/static/js/2.5adbbdf1.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/wanderlust/static/js/2.5adbbdf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fba124aedd234ef928ab",
+    "url": "/wanderlust/static/js/main.06586e0b.chunk.js"
+  },
+  {
+    "revision": "d2d7f36553c5bcac0505",
+    "url": "/wanderlust/static/js/runtime-main.51551550.js"
+  }
+]);
